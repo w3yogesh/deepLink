6dec9be24eb2ca413f889a64a0864fdc43975b96
Yogesh Kumar Sai <yogesh01041961@gmail.com>
2023-11-04 02:08:15 +0530

    Connect with MongoDb Atlas

--- server/server.js
+++ server/server.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const mongoose = require("mongoose");
+const PORT = 4000;
+const MONGO_URL = `mongodb+srv://Admin:sxYj5olZBKIUuroj@cluster0.erw4wx7.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose
+  .connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB is connected successfully"))
+  .catch((err) => console.error(err));
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`app listening on port ${port}`)
 })
