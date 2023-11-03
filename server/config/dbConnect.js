8b7d57d489ea90aba6aea8fa9e1ed80ecccfc871
Yogesh Kumar Sai <yogesh01041961@gmail.com>
2023-11-04 02:57:41 +0530

    Setup dotenv file for MongoDb connection, Install dotenv package for access enviroment file, Add dbConnect file in config to seprate db connection code,

---
+++ server/config/dbConnect.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const { DB_URL } = process.env;
+
+async function dbConnect() {
+
+  mongoose
+    .connect(DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      //useCreateIndex: true,
+    })
+
+    .then(() => {
+      console.log("Successfully connected to MongoDB Atlas!");
+    })
+    .catch((error) => {
+      console.log("Unable to connect to MongoDB Atlas!");
+      console.error(error);
+    });
+}
+
+module.exports = dbConnect;
--- server/server.js
+++ server/server.js
@@ -1,20 +1,19 @@
 const express = require('express')
 const app = express()
-const mongoose = require("mongoose");
-const PORT = 4000;
-const MONGO_URL = `mongodb+srv://Admin:sxYj5olZBKIUuroj@cluster0.erw4wx7.mongodb.net/?retryWrites=true&w=majority`;
 
-mongoose
-  .connect(MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+const connectDB = require('./config/dbConnect');
+require("dotenv").config();
+
+connectDB()
+  .then(() => {
+    const port = process.env.PORT || 4000;
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
   })
-  .then(() => console.log("MongoDB is connected successfully"))
-  .catch((err) => console.error(err));
-
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+  .catch((error) => {
+    console.error('Failed to start the server:', error);
+  });
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
