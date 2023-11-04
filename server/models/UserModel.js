4f0e9988a9804faaae781323e541a8cb35ed2958
Yogesh Kumar Sai <yogesh01041961@gmail.com>
2023-11-04 10:30:05 +0530

    userSchema added in userModel

---
+++ server/models/UserModel.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      lastName: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      username:{
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+      },
+      password: {
+        type: String,
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: new Date(),
+      },
+      updatedAt: {
+        type: Date,
+        default: new Date(),
+      },
+
+})
+
+module.exports = mongoose.model("User", userSchema);
