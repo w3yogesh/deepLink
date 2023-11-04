5d0a403c45cc52b094fd5aede1a620ee0a153252
Yogesh Kumar Sai <yogesh01041961@gmail.com>
2023-11-05 02:51:27 +0530

    Update Profile.jsx

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -6,71 +6,70 @@ import { useCookies } from "react-cookie";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 
-import ProfilePicture from './ProfilePicture';
-import Header from './Header';
-import Summary from './Summary';
-import Experience from './Experience';
-import Education from './Education';
-import Skills from './Skills';
+// import ProfilePicture from './ProfilePicture';
+// import Header from './Header';
+// import Summary from './Summary';
+// import Experience from './Experience';
+// import Education from './Education';
+// import Skills from './Skills';
 
 
 const Profile = () => {
     const navigate = useNavigate();
     const [cookies, removeCookie] = useCookies([]);
-    const [username, setUsername] = useState("");
-    const [Data, setData] = useState("");
+    const [userProfile, setUserProfile] = useState("");
 
     useEffect(() => {
-      const verifyCookie = async () => {
-        // if (!cookies.token) {
-        //   navigate("/login");
-        // }
+      const loadProfileData = async () => {
+        if (!cookies.token) {
+          navigate("/login");
+        }
         const { data } = await axios.post(
-          "http://localhost:4000",
+          "http://localhost:4000/profile",
           {},
           { withCredentials: true }
         );
-        const { status, user} = data;
-        setData(data);
-        setUsername(user);
+        const {status,user} = data;
+        if(!status){
+          toast("User Not Found", {
+            position: "top-right",
+          })
+          setTimeout(() => {
+            navigate("/login");
+          }, 2000);
+        }else{
+          setUserProfile(user);
+        }
         return status
-          ? toast(`Hello ${user}`, {
+          ? toast(`Hello ${user.username}`, {
               position: "top-right",
             })
           : (removeCookie("token"), navigate("/login"));
       };
-      verifyCookie();
+      loadProfileData();
     }, [cookies, navigate, removeCookie]);
     const Logout = () => {
       removeCookie("token");
       navigate("/");
     };
-
-    if (!cookies.token) {
-        return(<div>not login</div>)
-    }else{
-        return (
-          
-            <>
-              <div className="profile">
-        <ProfilePicture />
-        <Header />
-        <Summary />
-        <Experience />
-        <Education />
-        <Skills />
-      </div>
-                <h1>{Data.email}</h1>
-
-                <button onClick={Logout}>LOGOUT</button>
-                    
-              
-
-                <ToastContainer />
-            </>
-            
-          )
-    }
+    return (
+      
+        <>
+          {/* <div className="profile">
+            <ProfilePicture />
+            <Header />
+            <Summary />
+            <Experience />
+            <Education />
+            <Skills />
+          </div> */}
+            <h1>{userProfile.username}</h1>
+            <h1>{userProfile.email}</h1>
+            <button onClick={Logout}>LOGOUT</button>
+            <ToastContainer />
+        </>
+        
+      )
  
 }
 
