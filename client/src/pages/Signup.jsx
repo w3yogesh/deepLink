3e36efeb1e7df02fc623f5548794f9ff00e4f389
Sandipan19000 <sarkarsandipan19000@gmai.com>
2023-11-05 20:28:44 +0530

    signup page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 
 const Signup = () => {
