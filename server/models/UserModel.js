a8d1b81ee35775ad476100da1706463c537d614c
Yogesh Kumar Sai <yogesh01041961@gmail.com>
2023-11-24 14:19:01 +0530

    Added google signup

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -114,8 +114,8 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.pre("save", async function () {
-  this.password = await bcrypt.hash(this.password, 12);
-});
+// userSchema.pre("save", async function () {
+//   this.password = await bcrypt.hash(this.password, 12);
+// });
 
 module.exports = mongoose.model("User", userSchema);
