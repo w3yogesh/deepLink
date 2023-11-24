ff00da80c3c492fee5f18916de0ffa9a1f81db38
Sandipan19000 <sarkarsandipan19000@gmail.com>
2023-11-24 20:30:10 +0530

    drop connection in frontend page

--- client/src/components/MyConnections.jsx
+++ client/src/components/MyConnections.jsx
@@ -1,29 +1,35 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-function MyConnections(props) {
-    const [users, setUsers] = useState(props.connectedUser);
-    const userId = props.senderId;
+import LoadingForComponent from "./LoadingForComponent";
 
-    const handleDeleteMyConnection = async (senderId, receiverId)=>{
-      try {
-        const response = await axios.delete(`http://localhost:4000/api/deleteMyConnection/${senderId}/${receiverId}`);
-        const {status, message} = response.data;
-        if(status){
-          console.log(message);
-          setUsers(users => users.filter(user => user._id !== receiverId));
-        }else{
-          console.log(message);
-        }
-        
-      } catch (error) {
-        console.log(error);
+function MyConnections(props) {
+  const [users, setUsers] = useState(props.connectedUser);
+  const [isLoading, setIsLoading] = useState(true);
+  const userId = props.senderId;
+
+  const handleDeleteMyConnection = async (senderId, receiverId) => {
+    try {
+      const response = await axios.delete(
+        `http://localhost:4000/api/deleteMyConnection/${senderId}/${receiverId}`
+      );
+      const { status, message } = response.data;
+      if (status) {
+        console.log(message);
+        setUsers((users) => users.filter((user) => user._id !== receiverId));
+      } else {
+        console.log(message);
       }
+    } catch (error) {
+      console.log(error);
     }
+  };
   useEffect(() => {
     const fetchMyConnections = async () => {
       try {
-        const response = await axios.get(`http://localhost:4000/api/myConnections${userId}`);
+        const response = await axios.get(
+          `http://localhost:4000/api/myConnections${userId}`
+        );
         setUsers(response.data);
       } catch (error) {
         console.error("Error fetching users:", error);
@@ -31,34 +37,58 @@ function MyConnections(props) {
     };
     // fetchMyConnections();
     setUsers(props.connectedUser);
+    if(props.connectedUser.length > 0)  setIsLoading(false);
   }, [props.connectedUser]);
 
   return (
     <div>
-      <h4>My Conection</h4>
-      <ul className="user-cards">
-        {users.map((user) => (
-          <li className="user-card-list" key={user._id}>
-            <div className="user-card">
-              <div className="user-card-meta">
-                <div className="user-card-img profile-photo img">
-                  <img src={user.profileImage ? `http://localhost:4000/fetchProfileImage/${user.profileImage}` : "/images/user-profile-photo.svg"} alt="" />
+      {isLoading && <LoadingForComponent />}
+      {!isLoading && (
+        <div>
+          <h4>My Conection</h4>
+          <ul className="user-cards">
+            {users.map((user) => (
+              <li className="user-card-list" key={user._id}>
+                <div className="user-card">
+                  <div className="user-card-meta">
+                    <div className="user-card-img profile-photo img">
+                      <img
+                        src={
+                          user.profileImage
+                            ? `http://localhost:4000/fetchProfileImage/${user.profileImage}`
+                            : "/images/user-profile-photo.svg"
+                        }
+                        alt=""
+                      />
+                    </div>
+                    <div className="user-card-info">
+                      <Link to={`/userprofileview/${user._id}`}>
+                        {" "}
+                        <span className="user-card-name">
+                          {user.firstName} {user.lastName}
+                        </span>{" "}
+                      </Link>
+                      <span className="user-card-headline">
+                        {user.headline}
+                      </span>
+                      <span className="user-card-connection"></span>
+                    </div>
+                    <div className="user-card-action">
+                      <button
+                        onClick={() => {
+                          handleDeleteMyConnection(userId, user._id);
+                        }}
+                      >
+                        Drop
+                      </button>
+                    </div>
+                  </div>
                 </div>
-                <div className="user-card-info">
-                <Link to={`/userprofileview/${user._id}`}>  <span className="user-card-name">
-                    {user.firstName} {user.lastName}
-                  </span> </Link>
-                  <span className="user-card-headline">{user.headline}</span>
-                  <span className="user-card-connection"></span>
-                </div>
-                <div className="user-card-action">
-                <button onClick={()=>{handleDeleteMyConnection(userId,user._id)}}>Drop</button>
-                </div>
-              </div>
-            </div>
-          </li>
-        ))}
-      </ul>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
