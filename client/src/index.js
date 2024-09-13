d63e2bee2a9a0d4bcbe98ff59a28ec48a1082071
Yogesh Kumar Sai <yogesh01041961@gmail.com>
2024-09-13 15:20:23 +0530

    update app index

--- client/src/index.js
+++ client/src/index.js
@@ -7,10 +7,8 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
 );
 
