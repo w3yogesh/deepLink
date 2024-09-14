edf0c5f74cbc2eda269ac38af4e9cdb8d5b963da
Yogesh Kumar Sai <yogesh01041961@gmail.com>
2024-09-14 13:55:13 +0530

    update api2

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -105,12 +105,11 @@ module.exports.Login = async (req, res, next) => {
     }
     const token = createSecretToken(user._id);
     res.cookie("token", token, {
-      // sameSite: "None",
+      sameSite: "None",
       maxAge: 6000 * 1000,
-      // secure: process.env.NODE_ENV === "production",
-      // httpOnly: true,
-      // partitioned: true,
-      // Domain: process.env.NODE_ENV === 'production' ? 'deeplink-u52t.onrender.com' : 'localhost'
+      secure: process.env.NODE_ENV === "production",
+      httpOnly: true,
+      partitioned: true,
     });
     //const email = user.email;
     res
