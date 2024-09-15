e10e07faf4e32b8229a9d8d8872167351938e08d
Yogesh Kumar Sai <yogesh01041961@gmail.com>
2024-09-15 22:33:46 +0530

    remove console logs

--- server/middlewares/AuthMiddleware.js
+++ server/middlewares/AuthMiddleware.js
@@ -4,7 +4,6 @@ const jwt = require("jsonwebtoken");
 
 module.exports.userVerification = (req, res) => {
   const token = req.cookies.token;
-  console.log(token)
 
   if (!token) {
     return res.json({ status: false })
